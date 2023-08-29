fb0f160d74e50180c337b81e2164c5ed0cf5d873
Amr9876 <amr.aig.2007@gmail.com>
2023-08-29 14:30:08 +0300

    Initial Commit

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+
+	fmt.Println("Hello, World!")
+
+	loadError := godotenv.Load()
+
+	if loadError != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	portString := os.Getenv("PORT")
+
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.Get("/ready", handleReadiness)
+	v1Router.Get("/err", handleErr)
+
+	router.Mount("/v1", v1Router)
+
+	server := &http.Server{
+		Handler: router,
+		Addr:    ":" + portString,
+	}
+
+	log.Printf("Server starting on port %v", portString)
+	serverError := server.ListenAndServe()
+
+	if serverError != nil {
+		log.Fatal(serverError)
+	}
+
+}
